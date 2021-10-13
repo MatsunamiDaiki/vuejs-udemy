ec8944311ea5d78630b2235c0d35173a5bff8494
MatsunamiDaiki <lax.md0@gmail.com>
2021-10-13 14:33:23 +0900

    propsの勉強

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,38 @@
 <template>
   <div>
-    <LikeHeader></LikeHeader>
-    <LikeNumber></LikeNumber>
+    <LikeHeader>
+      <h3>はじめまして</h3>
+    </LikeHeader>
+    <LikeNumber :total-number="number" @my-click="incrementNumber"></LikeNumber>
+    <button @click="currentComponent = 'Home'">Home</button>
+    <button @click="currentComponent = 'About'">About</button>
+    <keep-alive>
+      <component :is="currentComponent"></component>
+    </keep-alive>
   </div>
 </template>
 
 <script>
 import LikeHeader from "./components/LikeHeader.vue";
+import About from "./components/About.vue";
+import Home from "./components/Home.vue";
 
 export default {
+  data() {
+    return {
+      number: 14,
+      currentComponent: "Home"
+    };
+  },
   components: {
-    LikeHeader
+    LikeHeader,
+    About,
+    Home
+  },
+  methods: {
+    incrementNumber(value) {
+      this.number = value;
+    }
   }
 };
 </script>
--- src/components/LikeNumber.vue
+++ src/components/LikeNumber.vue
@@ -1,22 +1,23 @@
 <template>
   <div>
-    <p>いいね({{ number }})</p>
+    <p>いいね({{ halfNumber }})</p>
     <button @click="increment">+1</button>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      number: 5
-    };
+  props: ["totalNumber"],
+  computed: {
+    halfNumber() {
+      return this.totalNumber / 2;
+    },
   },
   methods: {
     increment() {
-      this.number += 1;
-    }
-  }
+      this.$emit("my-click", this.totalNumber + 1);
+    },
+  },
 };
 </script>
 
