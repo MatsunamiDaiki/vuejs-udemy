6a127a93f51f14b7e3d7042f6ec50a15e8a92ade
MatsunamiDaiki <lax.md0@gmail.com>
2021-10-14 15:25:32 +0900

    フィルターの確認

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,24 @@
 <template>
-  <p v-border>Home</p>
+  <div>
+    <p v-border:dotted="{ width: '5px', color: 'red' }">Home</p>
+    <h2>{{title | upperCase }}</h2>
+  </div>
 </template>
+
+<script>
+export default {
+  data(){
+    return {
+      tmpData:"hello",
+      title:"Welcome to Tokyo",
+    }
+  },
+  directives: {
+    border(el, binding) {
+      el.style.borderWidth = binding.value.width;
+      el.style.borderColor = binding.value.color;
+      el.style.borderStyle = binding.arg;
+    },
+  },
+};
+</script>
